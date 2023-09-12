6df8b68fe25e9fbdc73db5e75ec1dad10e0f8e24
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2023-09-12 15:20:30 +0200

    Delete firebase connection + init postgres

--- client/src/utils/const.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from '../config/firebase';
-
-export const firebaseApp = initializeApp(firebaseConfig);
-export const firestore = getFirestore(firebaseApp);
---
+++ server/init/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS example (
+  id INT NOT NULL,
+  example_field varchar(250) NOT NULL,
+  PRIMARY KEY (id)
+);
